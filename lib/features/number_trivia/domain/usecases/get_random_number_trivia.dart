8a44a663379810ca9623a383812eff962096f25f
Douglas Ceron <ceron.douglas@gmail.com>
2022-10-05 15:13:51 -0300

    generation fromJson

--- lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -3,7 +3,6 @@ import 'package:clean_arch_tdd_number_trivia/core/usecases/usecase.dart';
 import 'package:clean_arch_tdd_number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:clean_arch_tdd_number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
 
 class GetRandomNumberTrivia implements Usecase<NumberTrivia, NoParams> {
   final NumberTriviaRepository repository;
--- test/features/number_trivia/domain/repositories/number_trivia_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:clean_arch_tdd_number_trivia/core/error/failures.dart';
-import 'package:clean_arch_tdd_number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
-import 'package:dartz/dartz.dart';
-
-abstract class NumberTriviaRepository {
-  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
-  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
-}
